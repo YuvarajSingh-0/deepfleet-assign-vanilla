10b7f9cab8e402ec408854d4149335f05d98c381
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2023-12-17 04:08:05 +0530

    Add comments

--- deepfleet-backend-assign/index.js
+++ deepfleet-backend-assign/index.js
@@ -1,15 +1,16 @@
-const { Client } = require('pg');
+const { Client } = require('pg'); // postgresql client for nodejs (used vercel db for postgresql)
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(cors())
+// middlewares for proper functioning of the server 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.use(cors()) // to allow cross origin requests
 
+// Database connection with environment variables
 const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
@@ -21,8 +22,11 @@ const client = new Client({
     },
 });
 
-client.connect();
+client.connect(); // connect to the database
 
+// API endpoints
+
+// GET request to fetch all the categories from the database
 app.get('/api/categories', (req, res) => {
     client.query('SELECT * from categories', (err, result) => {
         if (err) {
@@ -33,8 +37,11 @@ app.get('/api/categories', (req, res) => {
     });
 });
 
+// GET request to fetch all the products or products which match the category to query param category
 app.get('/api/products', (req, res) => {
     const category = req.query.category;
+
+    // If category is present in the query params, fetch products which match the category
     if (category) {
         client.query('SELECT * from products WHERE category = $1', [category], (err, result) => {
             if (err) {
@@ -43,7 +50,9 @@ app.get('/api/products', (req, res) => {
             }
             res.send(result.rows);
         });
-    } else {
+    } 
+    // If category is not present in the query params, fetch all the products
+    else {
         client.query('SELECT * from products', (err, result) => {
             if (err) {
                 console.error(err);
@@ -55,14 +64,18 @@ app.get('/api/products', (req, res) => {
 }
 );
 
+// POST request to add a new product to the database
 app.post('/api/products', (req, res) => {
     const { name, rate, category } = req.body;
+
+    // Fetch the gst of the category to which the product is being added
     client.query('SELECT gst from categories WHERE name = $1', [category], (err, result) => {
         if (err) {
             console.error(err);
             return;
         }
         const gst = result.rows[0].gst;
+        // Insert the product into the database
         client.query('INSERT INTO products(product, category, gst, rate) VALUES($1, $2, $3, $4)', [name, category, gst, rate], (err, result) => {
             if (err) {
                 console.error(err);
@@ -74,6 +87,7 @@ app.post('/api/products', (req, res) => {
 }
 );
 
+// POST request to add a new category to the database
 app.post('/api/category', (req, res) => {
     const { name,gst } = req.body;
     client.query('INSERT INTO categories(name, gst) VALUES($1, $2)', [name, gst], (err, result) => {
@@ -85,19 +99,9 @@ app.post('/api/category', (req, res) => {
     });
 });
 
-
-app.get('/', (req, res) => {
-    client.query('SELECT * from products', (err, result) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.send(result.rows);
-    });
-});
-
+// GET request to fetch all the invoices from the database
 app.get('/*', (req, res) => {
-    res.send('404');
+    res.send('No routes to this address');
 }
 );
 
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-// Fetch products from the database
+// defining a global variable to store the products from the database
+let productsFromDB = [];
+
+const productSelect = document.getElementById('productSelect');
+const categorySelect = document.getElementById('categorySelect');
+// Fetch categories from the database
 async function fetchCategories() {
     const response = await fetch('http://localhost:3000/api/categories');
     const categories = await response.json();
     return categories;
 }
 
+// Populate the select element with categories
 async function populateCategories() {
     const categories = await fetchCategories();
     console.log(categories);
-
+    
     // Populate the select element with categories
-    const categorySelect = document.getElementById('categorySelect');
     categories.forEach(category => {
         const option = document.createElement('option');
         option.value = category.name;
@@ -27,12 +32,15 @@ async function fetchProducts(category) {
     const products = await response.json();
     return products;
 }
-let productsFromDB = [];
-// Populate the products object with products from the database
+
+
+// Populate the select element with products based on the selected category
 async function populateProducts(category) {
     productsFromDB = await fetchProducts(category);
     console.log(productsFromDB, "products");
     productSelect.innerHTML = '';   // Clear previous options
+
+    // Populate the select element with products
     productsFromDB.forEach(product => {
         const option = document.createElement('option');
         option.value = product.product;
@@ -42,37 +50,35 @@ async function populateProducts(category) {
 }
 
 
-// Populate the select element with products based on the selected category
-const productSelect = document.getElementById('productSelect');
-
+// Add event listener to categorySelect to populate products when the category is changed
 categorySelect.addEventListener('change', () => {
     const selectedCategory = categorySelect.value;
     populateProducts(selectedCategory);
 });
 
-// Trigger change event on categorySelect to display products of default selected category
+// Trigger change event on categorySelect to display products of default selected category on page load
 categorySelect.dispatchEvent(new Event('change'));
 
+// handleAddToCart function to add the selected product to the cart
 async function handleAddToCart(e) {
     const selectedCategory = categorySelect.value;
     const selectedProduct = productSelect.value;
     const quantity = document.getElementById('quantity').value || 1;
 
-    // Find the selected product
+    // Find the selected product form all the products of selected category
     const products = productsFromDB;
     const product = products.find(product => product.product === selectedProduct);
     const rate = product.rate;
     const gst = product.gst;
 
-
-    // Create a new row in the table
     const table = document.getElementById('cartTable');
 
     // check if the product is already added to the cart
     for (let i = 1; i < table.rows.length; i++) {
         const productName = table.rows[i].cells[0].textContent;
+
+        // If the product is already added, update the quantity
         if (productName === selectedProduct) {
-            // Update the quantity
             const quantityCell = table.rows[i].cells[2];
             quantityCell.textContent = Number(quantityCell.textContent) + Number(quantity);
             return;
@@ -99,24 +105,25 @@ async function handleAddToCart(e) {
     gstCell.textContent = gst;
 }
 
+// generateBill function to calculate the total amount and display the bill
 function handleGenerateBill(e) {
-    // Iterate over each row of the cartTable and calculate the amount
     const cartTable = document.getElementById('cartTable');
     const billTable = document.getElementById('billTable');
     billTable.innerHTML = '<tr><th> Product Name</th><th>Category</th><th>Quantity</th><th>Price</th><th>GST</th><th>Total</th></tr > ';   // Clear previous bill
-    let totalAmount = 0
-
+    let totalAmount = 0 // total amount of cart items
+    
+    // Iterate over each row of the cartTable and calculate the amount
     for (let i = 1; i < cartTable.rows.length; i++) {
         const productName = cartTable.rows[i].cells[0].textContent;
         const quantity = cartTable.rows[i].cells[2].textContent || 1;
         const category = cartTable.rows[i].cells[1].textContent;
         const rate = cartTable.rows[i].cells[3].textContent;
         const gst = cartTable.rows[i].cells[4].textContent;
-        const amount = quantity * rate;
-        const gstAmount = (amount * gst) / 100;
-        const productAmount = amount + gstAmount;
+        const amount = quantity * rate; // amount without gst
+        const gstAmount = (amount * gst) / 100;   // gst amount
+        const productAmount = amount + gstAmount; // amount with gst
 
-        totalAmount = totalAmount + productAmount;
+        totalAmount = totalAmount + productAmount; // total amount of cart items
 
         // Create a new row in the billTable
         const newRow = billTable.insertRow();
@@ -141,21 +148,26 @@ function handleGenerateBill(e) {
         gstAmountCell.textContent = productAmount;
 
     }
+
+    // Display the total amount
     const totalAmountCell = document.getElementById('totalBill');
     totalAmountCell.textContent = totalAmount;
 
 }
 
+// handleAddProduct function to add a new product to the database
 function handleAddProduct(e) {
     const productName = document.getElementById('productName').value;
     const productCategory = document.getElementById('categorySelect').value;
     const productRate = document.getElementById('productRate').value;
 
+    // Check if all the fields are filled
     if (productName === '' || productCategory === '' || productRate === '') {
         alert("Please fill all the details");
         return;
     }
 
+    // Create a product object with the data
     const product = {
         name: productName,
         category: productCategory,
@@ -178,9 +190,12 @@ function handleAddProduct(e) {
         });
 }
 
+// handleAddCategory function to add a new category to the database
 function handleAddCategory(e) {
     const categoryName = document.getElementById('categoryName').value;
     const categoryGst = document.getElementById('categoryGst').value;
+
+    // Check if all the fields are filled
     if (categoryName === '' || categoryGst === '') {
         alert("Please fill all the details");
         return;
